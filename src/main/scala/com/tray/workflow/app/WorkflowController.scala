8be94d3b074b62a7f3335211cf4f9659b74fe12c
Oli Hall <metallic_suppression@hotmail.co.uk>
2016-08-17 23:57:07 +0100

    Add GET for all workflows

--- src/main/scala/com/tray/workflow/app/WorkflowController.scala
+++ src/main/scala/com/tray/workflow/app/WorkflowController.scala
@@ -1,8 +1,19 @@
 package com.tray.workflow.app
+import com.tray.workflow.persistence.{InMemoryWorkflowStore, WorkflowStore}
 import com.twitter.finatra.http.Controller
 class WorkflowController extends Controller {
 
+    private val store: WorkflowStore = new InMemoryWorkflowStore()
+
     get("/") { request: Request =>
         response.ok
+    }
+
+    //TODO support pagination
+    get("/workflows") { request: Request =>
+        val workflows = store.getAll()
+        response
+            .ok
+            .json(workflows)
     }
 }

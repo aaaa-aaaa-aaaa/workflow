1fafb3e50fba7973eeffe343ae1a4ebcdcb3c39b
Oli Hall <metallic_suppression@hotmail.co.uk>
2016-08-17 23:52:30 +0100

    Store map of executions within workflow

--- src/main/scala/com/tray/workflow/model/Workflow.scala
+++ src/main/scala/com/tray/workflow/model/Workflow.scala
@@ -1,6 +1,19 @@
 package com.tray.workflow.model
 
-case class Workflow(val id: String, val stepCount: Int) {
-    val id: String = id
-    val stepCount: Int = id
+case class Workflow(id: String, stepCount: Int,
+                    private var executions: Map[String, WorkflowExecution] = Map[String, WorkflowExecution]()) {
+
+    def getById(id: String): WorkflowExecution = {
+        executions.getOrElse(id, null)
+    }
+
+    def add(execution_id: String): WorkflowExecution = {
+        val execution = WorkflowExecution(execution_id, stepCount)
+        executions = executions + (execution.id -> execution)
+        execution
+    }
+
+    def getExecutions(): Iterable[WorkflowExecution] = {
+        executions.values
+    }
 }

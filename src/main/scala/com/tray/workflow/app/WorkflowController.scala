77c594cb927285482088928406d0d0200f322d9c
Oli Hall <metallic_suppression@hotmail.co.uk>
2016-08-17 23:57:51 +0100

    Add GET for single workflow by id

--- src/main/scala/com/tray/workflow/app/WorkflowController.scala
+++ src/main/scala/com/tray/workflow/app/WorkflowController.scala
@@ -1,5 +1,8 @@
 package com.tray.workflow.app
+import com.tray.workflow.domain.WorkflowGetRequest
+import com.tray.workflow.model.Workflow
 import com.tray.workflow.persistence.{InMemoryWorkflowStore, WorkflowStore}
+import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 class WorkflowController extends Controller {
 
@@ -15,5 +18,13 @@ class WorkflowController extends Controller {
         response
             .ok
             .json(workflows)
+    }
+
+    get("/workflows/:workflow_id") { request: WorkflowGetRequest =>
+        val workflow = store.getById(request.workflow_id)
+        workflow match {
+            case w: Workflow => response.ok.json(w)
+            case _ => response.notFound.jsonError(s"No Workflow found with id ${request.workflow_id}")
+        }
     }
 }

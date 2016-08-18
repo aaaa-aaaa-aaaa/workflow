8503f2bd625c59c4f1d229681190d707f2205280
Oli Hall <metallic_suppression@hotmail.co.uk>
2016-08-18 01:21:17 +0100

    use now() util method on LocalDateTime

--- src/main/scala/com/tray/workflow/model/WorkflowExecution.scala
+++ src/main/scala/com/tray/workflow/model/WorkflowExecution.scala
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger
 case class WorkflowExecution(id: String, maxSteps: Int) {
 
     private val currentStep: AtomicInteger = new AtomicInteger()
-    val created: LocalDateTime = new LocalDateTime()
+    val created: LocalDateTime = LocalDateTime.now()
 
     def next(): Option[Int] = {
         // TODO this is overall still not atomic
